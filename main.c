1a386df062587a5968a4e2440e888f09f999810e
Lucas Nagel <altingdesigns88@gmail.com>
2025-05-22 23:54:37 -0300

    black & white pieces set up

--- main.c
+++ main.c
@@ -11,11 +11,20 @@ king = 6
 
 typedef enum {
     EMPTY = 0,
-    PAWN,
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    QUEEN,
+
+    WHITE_PAWN = 1,
+    WHITE_ROOK = 2,
+    WHITE_KNIGHT = 3,
+    WHITE_BISHOP = 4,
+    WHITE_QUEEN = 5,
+    WHITE_KING = 6,
+
+    BLACK_PAWN = -1,
+    BLACK_ROOK = -2,
+    BLACK_KNIGHT = -3,
+    BLACK_BISHOP = -4,
+    BLACK_QUEEN = -5,
+    BLACK_KING = -6,
 } Piece;
 
 typedef struct {
@@ -31,15 +40,27 @@ void init_board(ChessGame *game) {
     }
 
     for (int j = 0; j < 8; j++) {
-        game->board[1][j] = PAWN;
-        game->board[6][j] = PAWN;
+        game->board[1][j] = BLACK_PAWN;
+        game->board[6][j] = WHITE_PAWN;
     } 
 
-    game->board[0][0] = ROOK;
-    game->board[0][7] = ROOK;
+    game->board[0][0] = BLACK_ROOK;
+    game->board[0][1] = BLACK_KNIGHT;
+    game->board[0][2] = BLACK_BISHOP;
+    game->board[0][3] = BLACK_QUEEN;
+    game->board[0][4] = BLACK_KING;
+    game->board[0][5] = BLACK_BISHOP;
+    game->board[0][6] = BLACK_KNIGHT;
+    game->board[0][7] = BLACK_ROOK;
 
-    game->board[7][0] = ROOK;
-    game->board[7][7] = ROOK;
+    game->board[7][0] = WHITE_ROOK;
+    game->board[7][1] = WHITE_KNIGHT;
+    game->board[7][2] = WHITE_BISHOP;
+    game->board[7][3] = WHITE_QUEEN;
+    game->board[7][4] = WHITE_KING;
+    game->board[7][5] = WHITE_BISHOP;
+    game->board[7][6] = WHITE_KNIGHT; 
+    game->board[7][7] = WHITE_ROOK;
 }
 
 void print_board(const ChessGame *game) {
